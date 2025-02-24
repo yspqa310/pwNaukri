77e4487cf9a5fa3eb4b6964a703ec983cdf24ee6
yakub.pasha <yspqa310@gmail.com>
2025-02-24 09:35:45 +0530

    naukri enhance

--- src/test/java/utilities/genericMethods.java
+++ src/test/java/utilities/genericMethods.java
@@ -21,6 +21,8 @@ import java.nio.file.Paths;
 import java.util.*;
 import java.util.List;
 
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
 public class genericMethods extends myBrowser {
     static Page page;
     public static Logger LOGGER = Logger.getLogger(genericMethods.class);
